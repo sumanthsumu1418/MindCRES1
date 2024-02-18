74925e26e9b1104729bd31eee35efaf2a811090b
Uttam25 <67574368+sumanthsumu1418@users.noreply.github.com>
2024-02-18 18:32:15 +0530

    changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -16,13 +16,14 @@ const Services = ({ mindCresContent }: any) => {
           <ul>
             <div className="lg:flex lg:flex-row justify-between">
               <div className="lg:w-1/4 relative mb-12 md:mb-16 lg:mb-16">
-                <p className=" text-secondary font-bold p-0 md:p-6 lg:p-6 w-48 md:-w-24 lg:w-8 text-[41px] md:text-[61px] lg:text-[61px]  unset_display_sm  text-left md:text-center lg:text-left">
+                <p className="text-secondary font-bold p-0 md:p-6 lg:p-6 w-48 md:-w-24 lg:w-8 text-[41px] md:text-[61px] lg:text-[61px]  unset_display_sm  text-left md:text-center lg:text-left">
                   {services.title}
                 </p>
               </div>
               <div className="grid service_sm grid-cols-2 gap-x-20 gap-y-10 md:gap-x-40 md:gap-y-16 lg:gap-x-40 lg:gap-y-16  ">
                 <div className="lg:w-[340px]  lg:mb-0">
-                  <li className="text-secondary h-[70px] md:h-[165px] lg:h-[165px] list-disc font-bold text-[32px] md:text-[39px] lg:text-[39px] ">
+                  <li className="text-secondary h-[70px] flex items-center mr-4  md:h-[165px] lg:h-[165px] list-none font-bold text-[32px] md:text-[39px] lg:text-[39px] ">
+                    <span className="flex w-[12px] relative h-[12px] bg-secondary rounded-full mr-4"></span>{" "}
                     {services.content[0].title}
                   </li>
                   <p className="text-[16px] md:text-[20px] lg:text-[20px] text-white">
@@ -30,7 +31,8 @@ const Services = ({ mindCresContent }: any) => {
                   </p>
                 </div>
                 <div className="lg:w-[340px]  ">
-                  <li className="text-secondary h-[115px] md:h-[165px] lg:h-[165px] font-bold  sm:list-disc text-[32px] md:text-[39px] lg:text-[39px] ">
+                  <li className="text-secondary flex items-base mr-4  h-[115px] md:h-[165px] lg:h-[165px] font-bold  list-none text-[32px] md:text-[39px] lg:text-[39px] ">
+                    <span className="flex w-[13px] relative h-[12px] bg-secondary mt-[5%] rounded-full mr-4"></span>{" "}
                     {services.content[1].title}
                   </li>
                   <p className="text-[16px] md:text-[20px] lg:text-[20px] text-white">
@@ -38,7 +40,8 @@ const Services = ({ mindCresContent }: any) => {
                   </p>
                 </div>
                 <div className="lg:w-[340px]  lg:mb-0">
-                  <li className="text-secondary h-[70px] md:h-[165px] lg:h-[165px] font-bold list-disc text-[32px] md:text-[39px] lg:text-[39px] ">
+                  <li className="text-secondary mb-8 md:mb-0 lg:mb-0 flex items-base mr-4  h-[70px] md:h-[165px] lg:h-[165px] font-bold list-none text-[32px] md:text-[39px] lg:text-[39px] ">
+                    <span className="flex w-[12px] relative h-[12px] bg-secondary rounded-full mt-[5%]  mr-4"></span>{" "}
                     {services.content[2].title}
                   </li>
                   <ol className="list-decimal text-white text-[16px] md:text-[20px] lg:text-[20px]">
@@ -50,7 +53,8 @@ const Services = ({ mindCresContent }: any) => {
                   </ol>
                 </div>
                 <div className="lg:w-[400px]  ">
-                  <li className="text-secondary h-[115px] md:h-[165px] lg:h-[165px] text-[32px] md:text-[39px] lg:text-[39px] font-bold list-disc">
+                  <li className="text-secondary flex items-base mr-4  h-[115px] md:h-[165px] lg:h-[165px] text-[32px] md:text-[39px] lg:text-[39px] font-bold list-none">
+                    <span className="flex w-[12px] relative h-[12px] bg-secondary mt-[5%] rounded-full mr-4"></span>{" "}
                     {services.content[3].title}
                   </li>
                   <div>
@@ -59,8 +63,9 @@ const Services = ({ mindCresContent }: any) => {
                     </p>
                   </div>
                 </div>
-                <div className="lg:w-[340px] ">
-                  <li className="text-secondary h-[70px] md:h-[165px] lg:h-[165px] font-bold list-disc text-[32px] md:text-[39px] lg:text-[39px] ">
+                <div className="lg:w-[400px] ">
+                  <li className="text-secondary h-[110px] md:h-[165px] lg:h-[165px] flex items-base mr-4  font-bold list-none text-[32px] md:text-[39px] lg:text-[39px] ">
+                    <span className="flex w-[12px] relative h-[12px] bg-secondary rounded-full mt-[5%] mr-4"></span>{" "}
                     {services.content[4].title}
                   </li>
                   <ol className="list-decimal text-white text-[16px] md:text-[20px] lg:text-[20px]">
