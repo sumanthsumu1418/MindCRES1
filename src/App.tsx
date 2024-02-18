2a14902b0ea429b1e4ffa1166889b096839cb0cf
sumanthsumu1418 <84310456+kurageGit@users.noreply.github.com>
2024-02-18 15:11:40 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import "./styles/tailwind.css";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 import WhyChooseUs from "./components/WhyChooseUs";
@@ -13,37 +13,28 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./Redux/store";
 import { ToastContainer, } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import firebase from 'firebase/app'
-import { RemoteConfig } from "firebase/remote-config";
+
+// Import the functions you need from the SDKs you need
+
+import { error } from "console";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const App: React.FC = () => { // Import the 'remote-config' module from the 'firebase/app' package
 
-  // const [configValue, setConfigValue] = React.useState(null);
 
-  // useEffect(() => {
-  //   if (!firebase.apps.length) {
-  //     firebase.initializeApp({
-  //       apiKey: "AIzaSyB0-2yNq9V9pYR3p2CgY5V8oZ9pR9XQ2t4",
-  //       authDomain: "react-landing-page-1c3e3.firebaseapp.com",
-  //       projectId: "react-landing-page-1c3e3",
-  //     })
-  //   }
-  // })
 
-  
 
-  // useEffect(() => {
-  //   const fetchConfig = async () => {
-  //     const response = await RemoteConfig().fetchAndActivate();
-  //     const greetingMessage = response.getString("greeting_message");
-  //     setConfigValue(greetingMessage);
-  //   };
-  //   fetchConfig();
-  // }, []);
-  
+
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <ToastContainer />
+        <ToastContainer />
         <Header />
         <Hero />
         <AboutUs />
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { fetchAndActivate, getRemoteConfig, getValue } from "firebase/remote-config";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRXsDPjf3u-dmdPTs4TcatPqKKEd0b9gk",
+  authDomain: "mindcreswebsite.firebaseapp.com",
+  databaseURL: "https://mindcreswebsite-default-rtdb.firebaseio.com",
+  projectId: "mindcreswebsite",
+  storageBucket: "mindcreswebsite.appspot.com",
+  messagingSenderId: "282039816576",
+  appId: "1:282039816576:web:2f53a86c335dee4965b165",
+  measurementId: "G-8CJ0X412VN"
+};
+
+const app = initializeApp(firebaseConfig);
+// console.log("fetchData getting called")
+const remoteConfig = getRemoteConfig()
+// fetchAndActivate(remoteConfig)
+//   .then(() => {
+//     return getRemoteConfig()
+//   })
+//   .then(() => {
+//     return getValue(remoteConfig, "mindcresContent")
+//   })
+//   .then((value) => {
+//     console.log("value==>", value)
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching remote config:", error);
+//   });
+
+// const isFetched = await fetchAndActivate(remoteConfig)
+
+// console.log("isFetched==>", isFetched)
+function Index() {
+  const [isFetched, setIsFetched] = useState(false);
+
+  useEffect(() => {
+    const fetchRemoteConfig = async () => {
+      try {
+        // const remoteConfig = firebase.remoteConfig();
+         const fetched = await fetchAndActivate(remoteConfig)
+        setIsFetched(fetched);
+       
+      } catch (error) {
+        console.error('Error fetching remote config:', error);
+        // Handle error fetching remote config
+      }
+      const value = getValue(remoteConfig, "mindcresContent")
+
+      console.log("value 1==>", JSON.parse(value._value))
+    };
+
+    fetchRemoteConfig();
+  }, []); // Run once on component mount
+
+  return (
+    <>
+      {isFetched ? <App /> : <div>Loading...</div>}
+    </>
+  );
+}
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Index />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
